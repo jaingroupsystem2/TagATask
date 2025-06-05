ebd2bd0d1a24a45c01ee4cf1b2817cda8e7960e8
jaingroupsystem2 <system2.thejaingroup@gmail.com>
2025-06-05 17:26:27 +0530

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,28 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { Sidebar } from './components/Sidebar/Sidebar';
 import TaskCreate from './components/TaskCreate';
+
 import { ToastContainer } from 'react-toastify';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import CalendarPage from './components/Calendar/CalendarPage';
+import Deadline from './components/Deadline/deadline';
 
 function App() {
-
   return (
     <>
-      <div>
-        <div className="app">
-          <Sidebar/>
-          <ToastContainer/>
-          <TaskCreate/>
-        </div>
+      <div className="app">
+        <BrowserRouter>
+          <Sidebar />
+          <ToastContainer />
+          <Routes>
+            <Route path="/" element={<TaskCreate />} />
+            <Route path="/calendar" element={<CalendarPage/>} />
+            <Route path="/deadline" element={<Deadline/>} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
